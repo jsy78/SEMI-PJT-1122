6cf353837eaef176613b87d4d4b36221666af88a
jsy78 <whtjd1541@gmail.com>
2022-11-09 22:28:33 +0900

    init project

---
+++ cafes/forms.py
@@ -0,0 +1,87 @@
+from django import forms
+from .models import Article, Review, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = [
+            "name",
+            "address",
+            "number",
+            "opening_hour",
+            "menu",
+            "parking",
+            "dayoff",
+            "image",
+        ]
+        labels = {
+            "name": "가게 이름",
+            "address": "주소",
+            "number": "전화번호",
+            "opening_hour": "개점 시간",
+            "menu": "메뉴",
+            "parking": "주차",
+            "dayoff": "휴일",
+            "image": "사진",
+        }
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = [
+            "title",
+            "content",
+            "rate",
+            "image",
+        ]
+        labels = {
+            "title": "제목",
+            "content": "내용",
+            "rate": "평점",
+            "image": "사진",
+        }
+        widgets = {
+            "rate": forms.NumberInput(
+                attrs={
+                    "step": "0.5",
+                    "max": "5.0",
+                    "min": "0.5",
+                }
+            ),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "댓글을 남겨보세요 💬",
+            }
+        ),
+    )
+
+    class Meta:
+        model = Comment
+        fields = [
+            "content",
+        ]
+
+
+class ReplyForm(forms.ModelForm):
+    content = forms.CharField(
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "대댓글을 남겨보세요 💬",
+            }
+        ),
+    )
+
+    class Meta:
+        model = Comment
+        fields = [
+            "content",
+        ]
---
+++ cafes/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = "cafes"
+
+urlpatterns = [
+    # path("", views.index, name="index"),
+    # path("<int:pk>/", views.detail, name="detail"),
+    # path("create/", views.create, name="create"),
+    # path("<int:pk>/update/", views.update, name="update"),
+    # path("<int:pk>/delete/", views.delete, name="delete"),
+    # path("<int:pk>/comments/", views.comment_create, name="comment_create"),
+    # path("<int:article_pk>/comments/<int:comment_pk>/delete/", views.comment_delete,name="comment_delete"),
+    # path("<int:article_pk>/likes/", views.likes, name="likes"),
+    # path("<int:article_pk>/bookmark/", views.bookmark, name="bookmark"),
+    # path("<int:article_pk>/comments/<int:comment_pk>/reply/", views.reply_create, name="reply_create"),
+]
