44ca02bb9d54f1f88f1f51fed49522021f091a79
baeseorim <bae--1440@gmail.com>
2022-11-11 14:59:58 +0900

    feat: accounts/templates 완성

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
-    # path("login/", views.login, name="login"),
+    path("login/", views.login, name="login"),
     # path("logout/", views.logout, name="logout"),
     # path("update/", views.update, name="update"),
     # path("password/", views.password, name="password"),
