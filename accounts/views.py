878a89879071012b1ea32cbefef98bde1f2017e7
nahkim <nahkim@student.42seoul.kr>
2022-11-11 13:58:54 +0900

    feat : 회원 가입 완료(backend)
    - 회원 가입
    - accounts/urls.py
    - accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cafes/index")
+    else:
+        form = UserCreationForm()
+        context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+
+# def login(request):
+#     form = AuthenticationForm()
+#     context = {"form": form}
+#     return render(request, "accounts/login.html", context)
