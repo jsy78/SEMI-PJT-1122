e01eb9b25f488a0e4fc35d0037570817ce46b75b
nahkim <nahkim@student.42seoul.kr>
2022-11-11 14:13:48 +0900

    fix : 로그인 에러 처리

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -17,7 +18,13 @@ def signup(request):
     return render(request, "accounts/signup.html", context)
 
 
-# def login(request):
-#     form = AuthenticationForm()
-#     context = {"form": form}
-#     return render(request, "accounts/login.html", context)
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            # return redirect(request.GET.get('next') or '')
+    else:
+        form = AuthenticationForm()
+        context = {"form": form}
+    return render(request, "accounts/login.html", context)
