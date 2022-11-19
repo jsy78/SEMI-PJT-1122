fc606033406d1e9ab32db58aed4b6eb1bd686ecc
kimheekimhee <khd128k@gmail.com>
2022-11-19 20:24:00 +0900

    feat : main page cafe list 카페 이름 옆에 평균 평점 나오게 추가

--- config/views.py
+++ config/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from cafes.models import Article, Review
 from django.views.decorators.http import require_safe
 from django.core.paginator import Paginator
+from django.db.models import Count, Avg
 
 
 @require_safe
 def main(request):
-    articles = Article.objects.order_by("-pk")
-
+    articles = Article.objects.order_by("-pk").annotate(reviews_count=Count('review')).annotate(average_rate=Avg('review__rate'))
     page = request.GET.get("page", "1")
     paginator = Paginator(articles, 6)
     page_obj = paginator.get_page(page)
