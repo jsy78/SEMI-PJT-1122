a0418be7e300813d85303f44cf4cdcead41f8063
nahkim <nahkim@student.42seoul.kr>
2022-11-21 02:55:34 +0900

    feat : 검색창 무한 스크롤 기능 추가
    - 검색시 무한 스크롤 기능 추가
    - main.html 불필요한 태그 삭제

--- config/views.py
+++ config/views.py
@@ -7,10 +7,18 @@ from django.db.models import Count, Avg
 
 @require_safe
 def main(request):
-    articles = Article.objects.order_by("-pk").annotate(reviews_count=Count('review')).annotate(average_rate=Avg('review__rate'))
+    articles = (
+        Article.objects.order_by("-pk")
+        .annotate(reviews_count=Count("review"))
+        .annotate(average_rate=Avg("review__rate"))
+    )
     page = request.GET.get("page", "1")
     paginator = Paginator(articles, 6)
     page_obj = paginator.get_page(page)
+
+    print("page_ob ", page_obj)
+    print("paginator ", paginator)
+    print("article ", articles)
 
     context = {
         "articles": articles,
